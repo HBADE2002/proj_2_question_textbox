ba47fe6f2e4929e1eaab515c1533390ec7061d05
HBADE2002 <hrishikeshbade007@gmail.com>
2024-04-05 19:09:37 +0530

    New Project, with question and answer features

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Question from './Question';
+
+const questions = [
+  {
+    id: 1,
+    title: 'What is Your Name?',
+    info: 'Your name is Hrishikesh Bade'
+  },
+  {
+    id: 2,
+    title: 'When is your birthday?',
+    info: 'Your birthday is on 20 July 2002 '
+  },
+  {
+    id: 3,
+    title: 'What is your Dream?',
+    info: 'Your dream is to be a successful software engineer'
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Project 2: FAQ/Accordion</h1>
+      <div className="container">
+        <h2>React Questions and Answers</h2>
+        <div className="questions">
+          {questions.map((question) => (
+            <Question key={question.id} question={question}></Question>
+          )) }
+        </div>
+      </div>
     </div>
   );
 }
